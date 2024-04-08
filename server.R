d3f8796abd00c6b4095122bf009970b79579609a
Edgar Ruiz <edgarruiztovar@gmail.com>
2024-04-07 20:27:15 -0600

    Finish the sheet1

--- server.R
+++ server.R
@@ -2,28 +2,80 @@ library(shiny)
 library(dplyr)
 library(lubridate)
 library(scales)
+library(ggplot2)
 
 # server
 server <- function(input, output, session) {
-    output$s1_table <- DT::renderDT({
-      id.cuenta <- get_id(input$cuenta1, cuentas)
-      id.trim <- get_id(ymd(input$trim1), trimestres)
-      id.ramo <- get_id(input$ramo1, operaciones)
+  ##############################################################################
+  # sheet1
+  # variables reactivas
+  #-----------------------------------------------------------------------------
+  s1.trim <- reactive({ymd(input$trim1)})
+  s1.cuenta <- reactive({input$cuenta1})
+  s1.ramo <- reactive({input$ramo1})
+  #-----------------------------------------------------------------------------
+  # tabla
+  #-----------------------------------------------------------------------------
+  output$s1_table <- DT::renderDT({
+    filter_df <- df %>% 
       # filtramos
-      filter_df <- db %>% 
-        filter(id_cuenta == id.cuenta,
-               id_trim == id.trim,
-               id_op %in% id.ramo) %>% 
-        mutate(Nombre = get_name(id_nom, instituciones),
-               Porcentaje = percent(importe/sum(importe), accuracy=0.01)) %>%
-        select(Nombre, importe, Porcentaje) %>% 
-        arrange(desc(importe))
-      # tomamos el total de lo antes filtrado
-      #total_s1 <- reactive({sum(filter_df$importe)})
-      return(filter_df)
-    })
+      filter(trimestre == s1.trim(),
+             cuenta == s1.cuenta(),
+             operacion %in% s1.ramo()) %>% 
+      # agrupamos por institución
+      group_by(institucion) %>% 
+      summarise(importe = sum(importe)) %>% 
+      # ponemos columna de porcentaje
+      mutate(porcentaje = percent(importe/sum(importe), accuracy=0.01)) %>%
+      # seleccionamos columnas
+      select(institucion, importe, porcentaje) %>% 
+      # ordenamos de manera descendente
+      arrange(desc(importe))
     
-    #output$s1_text <- renderText({
-     # total_s1()
-    #})
+    return(filter_df)
+  })
+  #-----------------------------------------------------------------------------
+  # gráfica
+  #-----------------------------------------------------------------------------
+  output$s1_graphic <- renderPlot({
+    filter_df <- df %>% 
+      # filtramos
+      filter(trimestre == s1.trim(),
+             cuenta == s1.cuenta(),
+             operacion %in% s1.ramo()) %>% 
+      # agrupamos por institución
+      group_by(institucion) %>% 
+      summarise(importe = sum(importe)) %>% 
+      # seleccionamos columnas
+      select(institucion, importe) %>% 
+      # ordenamos de menor a mayor
+      arrange(desc(importe)) %>%
+      head(10)  # Obtener las 10 primeras compañías
+    
+    # Gráfico de barras
+    ggplot(filter_df, aes(x = reorder(institucion, -importe), y = importe)) +
+      geom_bar(stat='identity', fill = "skyblue") +
+      labs(title = "Top 10 de Compañías", x = "Instituciones", y = "Importe") +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  })
+  #-----------------------------------------------------------------------------
+  ##############################################################################
+  
+  
+  ##############################################################################
+  # sheet2
+  s2.cuenta <- reactive({input$cuenta2})
+  s2.ramo <- reactive({input$ramo2})
+  s2.inst <- reactive({input$inst2})
+  ##############################################################################
+  
+  
+  ##############################################################################
+  # sheet3
+  s3.trim <- reactive({input$trim3})
+  s3.cuenta <- reactive({input$cuenta2})
+  s3.ramo <- reactive({input$ramo3})
+  s3.inst <- reactive({input$inst3})
+  ##############################################################################
+
 }
